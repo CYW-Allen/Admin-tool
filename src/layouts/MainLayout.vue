0ee1fd9984864688bd45fc4e76041d219b668a9e
Allen Chen <wen900445@gmail.com>
2023-10-23 09:12:06 +0800

    init proj

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header elevated class="bg-blue-2">
+      <q-bar class="glossy q-py-md">
+        <div class="col text-center text-weight-bold">Game Admin</div>
+        <q-space />
+        <q-icon name="fa-solid fa-gear" class="cursor-pointer" title="設定" />
+      </q-bar>
+
+      <!-- AppSetting component -->
+      <!-- Operpanel component -->
+    </q-header>
+
+    <q-page-container>
+      <q-page :style-fn="tweakPageH" class="bg-light-blue-1" style="overflow: hidden;">
+        <!-- function components -->
+      </q-page>
+      <!-- loading component -->
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+
+function tweakPageH(offset) {
+  return { height: offset ? `calc(100vh - ${offset}px)` : '100vh' };
+}
+</script>
